58acf65a31d9f6de4bc2363516ac9ab43f54b00d
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-24 22:19:34 +0200

    session wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,20 @@
 import type { AppProps } from 'next/app';
-
+import { Toaster } from "react-hot-toast"
+import { SessionProvider } from 'next-auth/react';
 import Layout from '@/components/Layout';
 import LoginModal from '@/components/LoginModal';
 import RegisterModal from '@/components/RegisterModal';
 import '@/styles/globals.css';
-// import Modal from '@/components/Modal';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-       {/* <Modal actionLabel="Submit" isOpen title="Test Modal" /> */}
+    <SessionProvider session={pageProps.session}>
+       <Toaster />
        <RegisterModal />
        <LoginModal />
        <Layout>
           <Component {...pageProps} />
        </Layout>
-    </>
+    </SessionProvider>
   );
 };
