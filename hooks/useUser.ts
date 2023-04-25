29f44a36ebd835a746b30146887f2cb8b69a55d7
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-25 17:43:54 +0200

    api route fix

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -2,7 +2,7 @@ import useSWR from "swr";
 import fetcher from "@/libs/fetcher";
 
 const useUser = (userId: string) => {
-  const { data, error, isLoading, mutate } = useSWR(userId ? `/api/user/${userId}` : null, fetcher);
+  const { data, error, isLoading, mutate } = useSWR(userId ? `/api/users/${userId}` : null, fetcher);
 
   return {
     data,
